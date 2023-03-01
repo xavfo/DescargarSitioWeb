279d95302007846137801f01552be164b9ed0801
Xavier Freire <40183534+xavfo@users.noreply.github.com>
2023-03-01 12:47:31 -0500

    Create descargar.sh

---
+++ descargar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Sitio: " $1
+echo " " 
+# One liner
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains $1 --no-parent $1
+#
+# Explained
+# wget \
+#      --recursive \ # Download the whole site.
+#      --page-requisites \ # Get all assets/elements (CSS/JS/images).
+#      --adjust-extension \ # Save files with .html on the end.
+#      --span-hosts \ # Include necessary assets from offsite as well.
+#      --convert-links \ # Update links to still work in the static version.
+#      --restrict-file-names=windows \ # Modify filenames to work in Windows as well.
+#      --domains yoursite.com \ # Do not follow links outside this domain.
+#      --no-parent \ # Don't follow links outside the directory you pass in.
+#          yoursite.com/whatever/path # The URL to download
